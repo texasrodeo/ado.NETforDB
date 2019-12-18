22cff4503332ae0bd06e851cf88c790afb2f5851
rakdo <kublenko_p_v@sc.vsu.ru>
2019-12-18 18:31:51 +0300

    Добавьте файлы проекта.

---
+++ ado.netForDB/DBUtils.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ado.netForDB
+{
+    public class DBUtils
+    {
+
+        private string connectionString;
+        
+
+        public DBUtils(string connectionString)
+        {
+            this.connectionString = connectionString;
+           
+        }
+
+        private SqlConnection GetConnection()
+        {
+           
+            return new SqlConnection(this.connectionString);          
+        }
+
+        public void GetInfo()
+        {
+            using(SqlConnection connection = GetConnection())
+            {
+                connection.Open();
+                Console.WriteLine("Свойства подключения:");
+                Console.WriteLine("\tСтрока подключения: {0}", connection.ConnectionString);
+                Console.WriteLine("\tБаза данных: {0}", connection.Database);
+                Console.WriteLine("\tСервер: {0}", connection.DataSource);
+                Console.WriteLine("\tВерсия сервера: {0}", connection.ServerVersion);
+                Console.WriteLine("\tСостояние: {0}", connection.State);
+                Console.WriteLine("\tWorkstationld: {0}", connection.WorkstationId);
+            }
+        }
+
+        public int ExecuteNonQuery(string query)
+        {
+            int number = 0;
+            using(SqlConnection connection = GetConnection())
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                try
+                {
+                    number = command.ExecuteNonQuery();
+                    Console.WriteLine("Успешное выполнение запроса");
+                }
+                catch(SqlException e)
+                {
+                    Console.WriteLine("Ошибка");
+                    Console.WriteLine(e.Number);
+                    Console.WriteLine(e.Message);
+                }
+                
+                
+            
+            return number;
+            }
+        }
+
+        private static string GetTableName(string query)        
+        {
+            string newQuery = query.ToUpper();
+            string[] split = query.Split();
+
+            return split[Array.IndexOf(split, "FROM") + 1];
+        }
+
+        //private static List<string> GetColumns(string query)
+        //{
+        //    string newQuery = query.ToUpper();
+        //    string[] split = query.Split();
+
+        //    Regex regex = new Regex(@"туп(\w*)", RegexOptions.IgnoreCase);
+        //}
+
+        public void ExecuteSelect(string query)
+        {
+            using(SqlConnection connection = GetConnection()) 
+            {
+                try
+                {
+                   
+                    connection.Open();
+                   
+                    DataTable table = connection.GetSchema("Columns", new string[] { null, null, GetTableName(query) });
+                   
+                    try
+                    {
+                        SqlCommand command = new SqlCommand(query, connection);
+                        SqlDataReader reader = command.ExecuteReader();
+                        if (reader.HasRows)
+                        {
+                            for (int i = 0; i < table.Rows.Count; i++)
+                            {
+                                Console.Write(table.Rows[i]["COLUMN_NAME"] + "       ");
+                            }
+                            Console.WriteLine();
+                            while (reader.Read())
+                            {
+                                for (int i = 0; i < table.Rows.Count; i++)
+                                {
+                                    Console.Write( reader.GetValue(i) + "       ");
+                                }
+                                Console.WriteLine();
+                            }
+                        }
+                    }
+                    catch (SqlException e)
+                    {
+                        Console.WriteLine("Ошибка");
+                        Console.WriteLine(e.Number);
+                        Console.WriteLine(e.Message);
+                    }
+
+                }
+                catch (InvalidOperationException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine(ex.InnerException);
+                }
+                
+                
+            }
+           
+        }
+
+    }
+}
---
+++ ado.netForDB/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace ado.netForDB
+{
+    class Program
+    {
+        static void ShowMenu(DBUtils dBUtils)
+        {
+            Console.WriteLine("----------------");
+            Console.WriteLine("Меню:");
+            Console.WriteLine("1 - Выполнить SELECT запрос");
+            Console.WriteLine("2 - Выполнить INSERT, DELETE, UPDATE запрос");
+            Console.WriteLine("----------------");
+            Console.WriteLine("Выберите операцию:");
+            int operation = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите запрос:");
+            string query = Console.ReadLine();
+            switch (operation)
+            {
+                case 1:
+                    dBUtils.ExecuteSelect(query);
+                    break;
+                case 2:
+                    dBUtils.ExecuteNonQuery(query);
+                    break;
+                default:
+                    Console.WriteLine("Неверно введена операция");
+                    break;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+            DBUtils dBUtils = new DBUtils(connectionString);
+
+            dBUtils.GetInfo();
+            Console.WriteLine();
+
+            while (true)
+            {
+                ShowMenu(dBUtils);
+            }                      
+        }
+    }
+}
